0c742be596f13bb7b9c074691e8963a2bfc06150
iot-workshop <stefanie.motzokan@hotmail.de>
2020-03-04 11:59:17 +0100

    Test Commit

--- webpage/src/App.vue
+++ webpage/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="humanIT" width="30%" src="./assets/hands.png">
-    <HelloWorld msg="Welcome to ShakeIT!"/>
+    <HelloWorld msg="Welcome to ShakeIT Test!"/>
     <p style="color: #999; font-size: small; font-weight: normal">ShakeIT is a simple and fun Internet of Things game, used for the development of an IoT showcase in purpose
       of a thesis.<br>
       &copy; Stefanie Motzokan, 2020
